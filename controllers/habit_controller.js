0a67f670df7f578604dcc5868b51670d52362249
sarita bagul <saritabagul06@gmail.com>
2023-07-13 23:08:33 +0530

    Added weekly habit tracker and updated the status of habit done,not done and none

--- controllers/habit_controller.js
+++ controllers/habit_controller.js
@@ -1,11 +1,12 @@
 const Habit = require('../models/habit'); 
 
+//get all the habits
 module.exports.home = async function(req,res){
     try{
-        const habit = await Habit.find({}).sort('-createdAt');
+        const habit = await Habit.find({}).sort('-createdAt');              
         return res.render('habit',{
             title:"Habit",
-            habit:habit
+            habit:habit,
         });
         
     }catch(err){
@@ -14,6 +15,8 @@ module.exports.home = async function(req,res){
     }
 }
 
+
+//create and save the habits in db
 module.exports.create = async function(req, res){
     try{
         let habit = await Habit.create({
@@ -38,7 +41,7 @@ module.exports.create = async function(req, res){
 }
 
 
-
+//delete or destroy habits from db
 module.exports.destroy = async function(req,res){
     try{
         const habit = await Habit.findById(req.params.id);      
@@ -59,3 +62,145 @@ module.exports.destroy = async function(req,res){
        
     }
 }
+
+
+
+// Get Habits From Database for details
+module.exports.details = async function(req,res){
+    try{
+
+        await Habit.find().select('-updatedAt -createdAt -__v').sort({ _id: -1 })
+        .then(habits => {
+            var days = [];
+            days.push(getD(0));
+            days.push(getD(1));
+            days.push(getD(2));
+            days.push(getD(3));
+            days.push(getD(4));
+            days.push(getD(5));
+            days.push(getD(6));
+            res.render('details', { habit: habits, days });
+        });
+    }catch(err){
+        console.log('Error to get habit details', err);
+        return redirect('back');
+    }
+      
+}
+
+
+    // Find the Date and Return the string Date
+    function getD(n) {
+        let d = new Date();
+        d.setDate(d.getDate() - n);
+        var newDate = d.toLocaleDateString('pt-br').split('/').reverse().join('-');
+        var day;
+        switch (d.getDay()) {
+            case 0: day = 'Sun';
+                break;
+            case 1: day = 'Mon';
+                break;
+            case 2: day = 'Tue';
+                break;
+            case 3: day = 'Wed';
+                break;
+            case 4: day = 'Thu';
+                break;
+            case 5: day = 'Fri';
+                break;
+            case 6: day = 'Sat';
+                break;
+        }
+        return { date: newDate, day };
+    }
+   
+    // Habit Status Update per Days
+    module.exports.habitStatus = async function(req,res){ 
+        try{
+            var d = req.query.date;
+            var id = req.query.id;
+            const habit =  await Habit.findById(id);
+            if(habit){
+                let dates = habit.dates;
+                let found = false;
+                let streak = habit.streak;
+                
+                dates.find(function (item, index) {
+                    if (item.date === d) {
+                        if (item.complete === 'yes') {
+                            item.complete = 'no';  
+                            streak--;                                                   
+                        }
+                        else if (item.complete === 'no') {
+                            item.complete = 'none';                           
+                                                    
+                        }
+                        else if (item.complete === 'none') {
+                            item.complete = 'yes'; 
+                            streak++;                           
+                        }
+                        found = true;
+                    }
+                })
+                if (!found) {
+                    dates.push({ date: d, complete: 'yes' });
+                    streak++;
+                }
+               
+                if(streak < 0){
+                    streak=0;
+                }
+              
+                habit.streak = streak;
+                habit.dates = dates;
+                habit.save();
+                return res.redirect('back');
+            }else{
+                return res.redirect('back');
+            }
+        }catch(err){
+            console.log("Habit status not updated", err);
+            res.status(500).send("Error updating habit status");
+        }   
+        // var d = req.query.date;
+        // var id = req.query.id;
+        // Habit.findById(id)
+        //     .then(habit => {
+        //         let dates = habit.dates;
+        //         let found = false;
+        //         let streak = habit.streak;
+        //         dates.find(function (item, index) {
+        //             if (item.date === d) {
+        //                 if (item.complete === 'yes') {
+        //                     item.complete = 'no';
+        //                     streak--;
+        //                 }
+        //                 else if (item.complete === 'no') {
+        //                     item.complete = 'none';
+        //                     streak--;
+        //                 }
+        //                 else if (item.complete === 'none') {
+        //                     item.complete = 'yes';
+        //                     streak++;
+        //                 }
+        //                 found = true;
+        //             }
+        //         })
+        //         if (!found) {
+        //             dates.push({ date: d, complete: 'yes' })
+        //         }
+        //         habit.dates = dates;
+        //         return habit.save();
+        //     })
+        //     .then(updatedHabit => {
+        //         // console.log(updatedHabit);
+        //         res.redirect('back');
+        //     })
+        //     .catch(err => {
+        //         console.log("Habit status not updated", err);
+        //         res.status(500).send("Error updating habit status");
+        //     });
+    }
+    
+    
+    
--- routes/habit.js
+++ routes/habit.js
@@ -5,5 +5,7 @@ const habitController = require('../controllers/habit_controller');
 
 router.post('/create',habitController.create);
 router.get('/destroy/:id',habitController.destroy);
+router.get('/details/',habitController.details);
+router.get('/habitStatus',habitController.habitStatus);
 
 module.exports = router;
