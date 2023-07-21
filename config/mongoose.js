86b4a93e20b163a7e74c2590ea3fd86f80690e30
sarita bagul <34671228+saritabagul@users.noreply.github.com>
2023-07-21 23:07:43 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/habit_tracker');
-// mongoose.connect('mongodb+srv://saritabagul06:nPaUESsfJKX1gpHO@cluster0.axvuzny.mongodb.net/?retryWrites=true&w=majority');
 
 const db = mongoose.connection;
 db.on('error',console.error.bind(console,"Error connecting in MongoDB"));
